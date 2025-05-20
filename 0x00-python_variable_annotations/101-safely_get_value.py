2be3df8347349fe90fdfc9752c2ee9abb1a98417
hyperbayse <hyperbayse@gmail.com>
2025-05-20 18:36:11 +0100

    correction

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,14 +2,14 @@
 """more involved type annotations"""
 
 
-from typing import TypeVar, Any, Mapping, Union, Optional
+from typing import TypeVar, Any, Mapping, Union
 
 
 # K = TypeVar('K')
 # V = TypeVar('V')
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: T | None = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     if key in dct:
         return dct[key]
     else:
