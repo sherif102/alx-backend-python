1490c4f31159dbc7438dbb832a15968f33609c35
hyperbayse <hyperbayse@gmail.com>
2025-05-20 14:21:06 +0100

    7-to_kv.py

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""complex types - string and int/float to tuple"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns of tuple of the supplied parameters"""
+    return (k, (v ** 2))
