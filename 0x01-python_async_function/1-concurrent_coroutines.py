065608db62b49bc7c00ce975576a015d2b1d2905
hyperbayse <hyperbayse@gmail.com>
2025-05-23 00:06:03 +0100

    annotation correction

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     """runs multiple coroutine functions simultaneously"""
-    result = []
+    result: list = []
     for i in list(range(0, n)):
         result.append(await wait_random(max_delay))
     return result
