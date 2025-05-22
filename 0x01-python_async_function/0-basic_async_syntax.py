ded4cd7951cb7a5704424f890e440ccb99f1b747
hyperbayse <hyperbayse@gmail.com>
2025-05-22 23:03:51 +0100

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""the basics of async"""
+
+
+import random, asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """asyncronously waith for a delay time"""
+    digit = random.random() * 100 / max_delay
+    await asyncio.sleep(digit)
+    return digit
