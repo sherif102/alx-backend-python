39925737748617c5d1dcc9ba3778a6df462ac531
hyperbayse <hyperbayse@gmail.com>
2025-05-23 00:20:39 +0100

    correction

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,15 +3,15 @@
 
 
 import asyncio
-from typing import Callable
+from typing import Callable, List
 
 
-wait_random: Callable = __import__('0-basic_async_syntax').wait_random
+wait_random: Callable[[int], asyncio.Future] = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """runs multiple coroutine functions simultaneously"""
-    result: list = []
+    result: List[float] = []
     for i in list(range(0, n)):
         result.append(await wait_random(max_delay))
     return result
