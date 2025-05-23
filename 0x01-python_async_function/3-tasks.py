055511e3360e771949dd2c66b6e492a9fec3ee43
hyperbayse <hyperbayse@gmail.com>
2025-05-23 10:50:17 +0100

    correction

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -11,6 +11,6 @@ wait_random: Callable[
     Awaitable[float]] = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """return an asyncio.Task"""
     return asyncio.Task(wait_random(max_delay))
