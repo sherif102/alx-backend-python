1b60ea33d06aaeab220259721e01fd6b6c57489b
hyperbayse <hyperbayse@gmail.com>
2025-05-24 13:51:16 +0100

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """async comprehensions"""
+
+
+from typing import List
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """generate an asyncrounous comprehension list"""
+    return [yielded async for yielded in async_generator()]
